241e753a95323bb3f54df17154efb60a50eed12f
STAN LEE <>
2021-03-21 02:15:47 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/2-useState-basics';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 // import Final from './tutorial/1-useState/final/1-error-example';
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      {/* <Final /> */}
     </div>
   );
 }
